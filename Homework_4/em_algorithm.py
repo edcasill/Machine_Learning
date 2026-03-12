060e8c24a7d14d9679bde13b33ccd69c09d19197
edcasill <acasillasval@gmail.com>
2026-03-11 23:32:50 -0600

    added multiple tries to get the best result

--- Homework_4/em_algorithm.py
+++ Homework_4/em_algorithm.py
@@ -59,43 +59,55 @@ class em_algorithm:
 
         return new_pi, mu_hat, sigma
 
-    def fit_em(self, X:jnp.ndarray, iterations: int = 100, tolerance: float=1e-4):
+    def fit_em(self, X:jnp.ndarray, iterations: int = 100, tolerance: float=1e-4, n_init: int = 7):
         """
         iterate model until convergence (step 4 of algorithm)
         """
         N, d = X.shape
+        best_pi = None
+        best_mu = None
+        best_sigma = None
+        best_log_likelihood = -jnp.inf
 
-        # pi: we initialize pi at 0.5, same for both clusters
-        pi = jnp.array([0.5, 0.5]) 
+        # diferent intialization to get the best result
+        for em_try in range(n_init):
+            # pi: we initialize pi at 0.5, same for both clusters
+            pi = jnp.array([0.5, 0.5]) 
 
-        # mu: random points on the dataset, they gonna be the center
-        self.key, subkey = jax.random.split(self.key)
-        index = jax.random.choice(subkey, jnp.arange(N), shape=(2,), replace=False)
-        mu = X[index]
+            # mu: random points on the dataset, they gonna be the center
+            self.key, subkey = jax.random.split(self.key)
+            index = jax.random.choice(subkey, jnp.arange(N), shape=(2,), replace=False)
+            mu = X[index]
 
-        # sigma: we initialize identity matrix for both sigmas
-        sigma = jnp.stack([jnp.eye(d), jnp.eye(d)])
+            # sigma: we initialize identity matrix for both sigmas
+            sigma = jnp.stack([jnp.eye(d), jnp.eye(d)])
 
-        log_likelihood_old = -jnp.inf
+            log_likelihood_old = -jnp.inf
 
-        for i in range(iterations):
-            # step 2 Calculate resposibilities
-            gamma, log_likelihood = self.expectation_step(X, pi, mu, sigma)
+            for i in range(iterations):
+                # step 2 Calculate resposibilities
+                gamma, log_likelihood = self.expectation_step(X, pi, mu, sigma)
 
-            # convergence condition: if the change is minimum, we're done
-            diff = jnp.abs(log_likelihood - log_likelihood_old)
-            if diff < tolerance:
-                print(f"Convergence reach at iteration {i+1} | Log-Likelihood: {log_likelihood:.4f}")
-                break
+                # convergence condition: if the change is minimum, we're done
+                diff = jnp.abs(log_likelihood - log_likelihood_old)
+                if diff < tolerance:
+                    print(f"Convergence reach at iteration {i+1} | Log-Likelihood: {log_likelihood:.4f}")
+                    break
 
-            log_likelihood_old = log_likelihood
+                log_likelihood_old = log_likelihood
 
-            # step3: update parameters
-            pi, mu, sigma = self.maximization_step(X, gamma)
+                # step3: update parameters
+                pi, mu, sigma = self.maximization_step(X, gamma)
 
-        self.pi = pi
-        self.mu = mu
-        self.sigma = sigma
+            if log_likelihood > best_log_likelihood:
+                best_log_likelihood = log_likelihood
+                best_pi = pi
+                best_mu = mu
+                best_sigma = sigma
+
+        self.pi = best_pi
+        self.mu = best_mu
+        self.sigma = best_sigma
 
         return self
 
