794b38f574e4305510335bf6023c6fd3a1d537e7
edcasill <acasillasval@gmail.com>
2026-04-02 00:58:06 -0600

    forward prop

--- Homework_5/MLP_matrix.py
+++ Homework_5/MLP_matrix.py
@@ -9,29 +9,50 @@ class Multilayer_Perceptron_Matrix():
     """
     Multilayer pereptron ussing the classic matrix implementation
     """
-    def __init__(self, X, h_layers=1, seed=73):
+    def __init__(self, X, layers=[0, 128, 10], seed=73):
         key = jax.random.PRNGKey(seed)
-        keys = jax.random.split(key, len(h_layers) - 1)
+        self.keys = jax.random.split(key, len(layers) - 1)
+        layers[0] = X.shape[1]  # input layer
+        self.layers = layers
         self.params = {}
 
-        for i in range(len(h_layers) - 1):
-            dim_in = h_layers[i]
-            dim_out = h_layers[i+1]
+        # this is implemented if in a future we want to add more hiden layers
+        for i in range(len(self.layers) - 1):
+            dim_in = self.layers[i]
+            dim_out = self.layers[i+1]
             
-            # Inicialización He
-            self.params[f'W{i+1}'] = jax.random.normal(keys[i], (dim_in, dim_out)) * jnp.sqrt(2.0 / dim_in)
+            # Weights and bias initialization for each hidden layer and the output layer
+            self.params[f'W{i+1}'] = jax.random.normal(self.keys[i], (dim_in, dim_out)) * jnp.sqrt(2.0 / dim_in)
             self.params[f'b{i+1}'] = jnp.zeros((1, dim_out))
 
-    def forward_propagation():
+    def forward_propagation(self, X):
         """
         """
+        self.neuron_state = []
+        activations_hidden = X
+        # this is because I'm keeping 2 values for each layer (bias an weigth),
+        # obtaining the int number from the divison give me the quantity of real layers, not it's elements
+        total_layers = len(self.params) // 2
+
+        # forward result on hidden layer
+        for i in range(1, total_layers): 
+            Z_hidden = (activations_hidden @ self.params[f'W{i}']) + self.params[f'b{i}']
+            self.neuron_state.append((activations_hidden, self.params[f'W{i}'], Z_hidden)) # keep the state of each neuron
+            activations_hidden = self.sigmoid(Z_hidden)
+        
+        # forward result on output layer
+        Z_out = (activations_hidden @ self.params[f'W{total_layers}']) + self.params[f'b{total_layers}']
+        self.neuron_state.append((activations_hidden, self.params[f'W{total_layers}'], Z_out)) # keep the state of each neuron
+        self.y_out = self.softmax(Z_out)
+        return self
     
-    def backward_propafation():
+    def backward_propagation(self):
         """
         """
+        return self
     
     # activation functions
-
+    @staticmethod
     def sigmoid(Z):
         """
         Sigmoid activation funtion
@@ -43,7 +64,8 @@ class Multilayer_Perceptron_Matrix():
             _type_: _description_
         """
         return 1 / (1 + jnp.exp(-Z))
-    
+
+    @staticmethod
     def sigmoid_derivative(Z):
         """
         Sigmoid derivative
@@ -55,7 +77,8 @@ class Multilayer_Perceptron_Matrix():
             _type_: _description_
         """
         return (jnp.exp(-Z)) / (1 + jnp.exp(-Z))**2
-    
+
+    @staticmethod
     def softmax(Z):
         """
         Lineal transformation, previous layer multiplied by weigths plus the bias
@@ -68,7 +91,8 @@ class Multilayer_Perceptron_Matrix():
         """
         exp_Z = jnp.exp(Z - jnp.max(Z, axis=1, keepdims=True))
         return exp_Z / jnp.sum(exp_Z, axis=1, keepdims=True)
-    
+
+    @staticmethod
     def relu(Z):
         """
         Rectified Linear Unit for non linearity into the net
@@ -81,6 +105,7 @@ class Multilayer_Perceptron_Matrix():
         """
         return jnp.maximum(0, Z)
 
+    @staticmethod
     def relu_derivada(Z):
         """
         Relu derivate, is 1 if Z > 0, 0 otherwise
